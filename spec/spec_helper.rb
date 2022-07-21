f62477aa5781bd7a1463bc13b1bd2353e861b3db
Joseph Mastey <302011+jmmastey@users.noreply.github.com>
2022-07-21 09:25:07 -0500

    non-rails-dependent syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,7 +26,7 @@ RSpec.configure do |config|
   # is tagged with `:focus`, all examples get run. RSpec also provides
   # aliases for `it`, `describe`, and `context` that include `:focus`
   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
-  if ENV['CI'].to_b
+  if ENV['CI'] == "true"
     config.filter_run_when_matching :focus => lambda { raise ArgumentError, "Don't filter tests on checked in code." }
   else
     config.filter_run_when_matching :focus
